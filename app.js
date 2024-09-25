8144d337b2ff9f80fb9eb6a9b25972b546b4243a
akemashita <134128275+akemashita@users.noreply.github.com>
2024-09-25 02:24:28 +0000

    都道府県ごとの変化率を計算する処理の実装

--- app.js
+++ app.js
@@ -6,21 +6,45 @@ const FS = require('fs');
 const READLINE = require('readline');
 const RS = FS.ReadStream('./popu-pref.csv');
 const RL = READLINE.createInterface({ 'input': RS, 'output': {} });
+const MAP = new Map(); // key:都道府県 value:集計データのオブジェクト
 
 RL.on('line', (lineString) => {
-    // ２　2010年と2015年のデータを選ぶ
-    const columns = lineString.split(',');
-    const year = parseInt(columns[0]);
-    const prefecture = columns[2];
-    const popu = parseInt(columns[7]);
-    if (year === 2010 || year === 2015) {
-        console.log(year);
-        console.log(prefecture);
-        console.log(popu);
-    }
-});
-RL.resume();
+  // ２　2010年と2015年のデータを選ぶ
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[2];
+  const popu = parseInt(columns[7]);
 
-// ３　都道府県ごとの変化率を計算する
+  if (year === 2010 || year === 2015) {
+    let value = MAP.get(prefecture);
+    if (!value) {
+      value = {
+        popu10: 0,
+        popu15: 0,
+        change: null
+      };
+    }
+
+    if (year === 2010) {
+      value.popu10 += popu;
+    }
+    if (year === 2015) {
+      value.popu15 += popu;
+    }
+    
+    MAP.set(prefecture, value);
+  }
+});
+
+RL.resume();
+RL.on('close', () => {
+  // ３　都道府県ごとの変化率を計算する
+  for (let pair of MAP) {
+    const VALUE = pair[1];
+    VALUE.change = VALUE.popu15 / VALUE.popu10;
+  }
+  console.log(MAP);
+});
+
 // ４　変化率ごとに並べる
 // ５　並べられたものを表示する
